93702f0296d9ffa7ad2c38eeab3340f17d3e6637
Chewy <chinong1234@gmail.com>
2025-07-04 01:00:02 +0800

    remove useless code

--- FourtitudeMiddleware/Controllers/BaseController.cs
+++ FourtitudeMiddleware/Controllers/BaseController.cs
@@ -6,36 +6,6 @@ namespace FourtitudeMiddleware.Controllers
     [ApiController]
     public abstract class BaseController : ControllerBase
     {
-        protected ActionResult<T> SuccessResponse<T>(T response)
-            where T : class
-        {
-            var resultProp = typeof(T).GetProperty("Result");
-            if (resultProp != null)
-            {
-                resultProp.SetValue(response, 1);
-            }
-            return Ok(response);
-        }
-
-        protected ActionResult<T> ErrorResponse<T>(string message, int statusCode = 400)
-            where T : class, new()
-        {
-            var response = new T();
-            var resultProp = typeof(T).GetProperty("Result");
-            var messageProp = typeof(T).GetProperty("ResultMessage");
-
-            if (resultProp != null)
-            {
-                resultProp.SetValue(response, 0);
-            }
-            if (messageProp != null)
-            {
-                messageProp.SetValue(response, message);
-            }
-
-            return StatusCode(statusCode, response);
-        }
-
         protected ActionResult<T> HandleResponse<T>(T response)
             where T : class
         {
@@ -47,15 +17,21 @@ namespace FourtitudeMiddleware.Controllers
             if (isSuccess)
             {
                 if (messageProp != null)
+                {
                     messageProp.SetValue(response, null);
+                }
                 return StatusCode(200, response);
             }
             else
             {
                 if (resultProp != null)
+                {
                     resultProp.SetValue(response, 0);
+                }
                 if (messageProp != null && messageProp.GetValue(response) == null)
+                {
                     messageProp.SetValue(response, "An error occurred.");
+                }
                 return StatusCode(400, response);
             }
         }
